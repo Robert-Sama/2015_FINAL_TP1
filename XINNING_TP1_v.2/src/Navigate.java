24869c6d0ddeafbe4626d59b2b8073c6a40eb607
panini-xxn <132922792+panini-xxn@users.noreply.github.com>
2023-10-26 22:18:53 -0400

    Update Navigate.java

--- XINNING_TP1_v.2/src/Navigate.java
+++ XINNING_TP1_v.2/src/Navigate.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 import java.util.*;
 
 /*
- * We make this class to make a recrusive function that will keep on looking at neighbors until ther's no more words
+ * We make this class to make a recursive function that will keep on looking at neighbours in the letter matrix
+ * until there are no more words.
  */
 
 public class Navigate {
@@ -15,47 +16,44 @@ public class Navigate {
     int yCoord;
     List<String> foundWords;
 
-    //We translate what's in main in here
+    //We translate what's in the Main class in here
     //We take a word we're looking for, the matrix and the dimensions of the matrix
     public Navigate(String word, List<List<String>> matrice, int colIndex, int rowIndex) {
         this.word = word;
         this.matrice = matrice;
         this.colIndex = colIndex;
         this.rowIndex = rowIndex;
-        //this.c = c;
-        //this.r = r;
         foundWords = new ArrayList<>();
     }
 
     public void findWords() {
         for (int r = 0; r < rowIndex; r++) {
             for (int c = 0; c < colIndex; c++) {
-                byakugan(0, c, r, new ArrayList<>());
+                searchLoop(0, c, r, new ArrayList<>());
             }
         }
     }
 
-    public void byakugan(int lettrePos, int c, int r, List<String> alphonse) {
-    	//We want to loop while the indexs of the matrix and of the letters are in bound
+    public void searchLoop(int lettrePos, int c, int r, List<String> alphonse) {
+    	//We want to loop while the indexes of the matrix and of the letters are in bound
+        //Alphonse is a list of strings, it stores the coordinates of found letters of the searched word.
         if (c >= 0 && c < colIndex && r >= 0 && r < rowIndex && lettrePos < word.length()) {
         	//We get the letter at the position lettrePos
             String lettre = Character.toString(word.charAt(lettrePos));
-            //IF our letter is the same as the one in the matrix then 
+            //If our letter is the same as the one in the matrix.
             if (lettre.equals(matrice.get(c).get(r))) {
-            	//Change alhponse !!!
-                //alphonse.add(word);
-                //We give alphonse the big task of looking over positions, he can do it!
+                //Alphonse adds the coordinates of that letter.
                 alphonse.add("(" + c + ", " + r + ")");
                 if (lettrePos == word.length() - 1) {
                     foundWords.add(String.join("->", alphonse));
-                } else { //IF we didn't find a letter, we still want to continue the search as long as the indexs are valid
+                } else { //IF we didn't find a letter, we still want to continue the search as long as the indexes are valid
                     for (int yOffset = -1; yOffset <= 1; yOffset++) {
                         for (int xOffset = -1; xOffset <= 1; xOffset++) {
                             if (yOffset != 0 || xOffset != 0) {
                                 int newC = c + xOffset;
                                 int newR = r + yOffset;
-                                //We call alphonse again, cause we didn't find anything
-                                byakugan(lettrePos + 1, newC, newR, new ArrayList<>(alphonse));
+                                //We call alphonse again, because we didn't find anything
+                                searchLoop(lettrePos + 1, newC, newR, new ArrayList<>(alphonse));
                             }
                         }
                     }
@@ -63,7 +61,7 @@ public class Navigate {
             }
         }
     }
-    //We print all the juicy positions that alphonse gave us
+    //We print all the positions that Alphonse gave us
     public void printFoundWords() {
         for (String foundWord : foundWords) {
             System.out.println(word + foundWord );
